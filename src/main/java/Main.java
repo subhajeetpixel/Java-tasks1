1a8be9fc379a35de97b888b8bfa99899748e1f43
Subhajeet Naskar <153799680+subhajeetpixel@users.noreply.github.com>
2024-07-27 15:20:47 +0000

    Updated calculator code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,38 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter first number:");
+    double num1 = scanner.nextDouble();
+    System.out.println("Enter operator (+, -, *, /):");
+    char operator = scanner.next().charAt(0);
+    System.out.println("Enter second number:");
+    double num2 = scanner.nextDouble();
+    scanner.close();
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    double result;
+    switch (operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        if (num2 == 0) {
+          System.out.println("Cannot divide by zero.");
+          return;
+        }
+        result = num1 / num2;
+        break;
+      default:
+        System.out.println("Invalid operator.");
+        return;
+    }
+    System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+  }
 }
